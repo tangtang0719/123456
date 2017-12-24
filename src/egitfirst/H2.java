779bd99582ae3a9e1c3a5adacb926d5a9d60cbbb
zgs <915894435@qq.com>
2017-12-24 19:52:38 +0800

    project push

---
+++ src/egitfirst/H2.java
@@ -0,0 +1,10 @@
+package egitfirst;
+
+public class H2 {
+	public void h2() {
+		System.out.println("h2 h2 h2");
+		System.out.println("h2 h2 h2");
+		System.out.println("h2 h2 h2");
+		System.out.println("h2 h2 h2");
+	}
+}
